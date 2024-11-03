72287cc083a014617c15d600f5c1b840becc7f4e
Valentinka2222 <xamalyan89@gmail.com>
2024-11-03 16:32:16 +0200

    moved App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
 import { Box, Typography, CssBaseline } from '@mui/material';
 
-import Search from '../inputs/Search';
-import theme from '../../theme';
-import WorkersList from '../workers/Workers';
-import WorkerDetails from '../details/WorkerDetails';
-import type { SortOrder } from '../../entities/Workers';
+import Search from './inputs/Search';
+import theme from '../theme';
+import WorkersList from './workers/Workers';
+import WorkerDetails from './details/WorkerDetails';
+import type { SortOrder } from '../entities/Workers';
 
-import '../../index.scss';
+import '../index.scss';
 import './App.scss';
 
 export default function App() {

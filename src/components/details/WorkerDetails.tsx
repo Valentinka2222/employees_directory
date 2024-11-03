470689088557bc983d868467510fab5b9dee1e0b
Valentinka2222 <xamalyan89@gmail.com>
2024-11-03 15:40:26 +0200

    1.created readme. file; 2. added logic after reloading page we will save prev data

--- src/components/details/WorkerDetails.tsx
+++ src/components/details/WorkerDetails.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
 import { useParams, Link } from 'react-router-dom';
+
 import { Box, Stack, Typography, Avatar } from '@mui/material';
 
+import { fetchWorkersAction } from '../../redux/reducer/workersReducer'; // Adjust path as needed
 import { Worker } from '../../entities/Workers';
-import { RootState } from '../../redux/store/store';
+import { RootState, AppDispatch } from '../../redux/store/store';
 
 import star from '../../assets/star.png';
 import phone from '../../assets/phone.png';
@@ -15,58 +17,67 @@ import './workerDetails.scss';
 
 const WorkerDetails: React.FC = () => {
   const { id } = useParams<{ id: string }>();
+  const dispatch: AppDispatch = useDispatch();
   const { workers } = useSelector((state: RootState) => state.workers);
-  const worker = workers.find((worker: Worker) => Number(worker.id) === parseInt(id || '', 10));
+  const worker = workers.find((worker: Worker) => worker.id === id);
 
-  if (!worker) {
-    return <Typography variant="h5">Worker not found</Typography>;
-  }
+  useEffect(() => {
+    if (!workers.length) {
+      dispatch(fetchWorkersAction()); // Fetch workers if not available in Redux store
+    }
+  }, [dispatch, workers]);
 
   return (
     <Box className="worker-details">
       <Box className="worker-details__header">
         <Link to="/">
-          <img src={right_arrow} alt="Birthday" />
+          <img src={right_arrow} alt="Go back" />
         </Link>
       </Box>
-      <Stack className="worker-details__info-card" spacing={2}>
-        <Avatar alt={worker.name} src={worker.avatar} />
-        <Stack direction="column">
-          <Typography
-            className="worker-details__info-card__name"
-            variant="subtitle2"
-            color="text.primary"
-          >
-            {worker.name}{' '}
+      {worker && (
+        <Stack className="worker-details__info-card" spacing={2}>
+          <Avatar alt={worker.name} src={worker.avatar} />
+          <Stack direction="column">
             <Typography
-              component="span"
+              className="worker-details__info-card__name"
+              variant="subtitle2"
+              color="text.primary"
+            >
+              {worker.name}{' '}
+              <Typography
+                component="span"
+                variant="body2"
+                color="text.secondary"
+                className="worker-list__item-details-tag"
+              >
+                {worker.tag}
+              </Typography>
+            </Typography>
+            <Typography
+              className="worker-details__info-card__position"
+              textAlign="center"
               variant="body2"
               color="text.secondary"
-              className="worker-list__item-details-tag"
             >
-              {worker.tag}
+              {worker.position}
             </Typography>
-          </Typography>
-          <Typography
-            className="worker-details__info-card__position"
-            textAlign="center"
-            variant="body2"
-            color="text.secondary"
-          >
-            {worker.position}
-          </Typography>
+          </Stack>
         </Stack>
-      </Stack>
-      <Box>
-        <Box className="worker-details__details-item">
-          <img src={star} alt="Birthday" />
-          <Typography variant="body1">{moment(worker.birthDate).format('D MMMM YYYY')}</Typography>
+      )}
+      {worker && (
+        <Box>
+          <Box className="worker-details__details-item">
+            <img src={star} alt="Birthday" />
+            <Typography variant="body1">
+              {moment(worker.birthDate).format('D MMMM YYYY')}
+            </Typography>
+          </Box>
+          <Box className="worker-details__details-item">
+            <img height="28px" src={phone} alt="Phone" />
+            <Typography variant="body1">{worker.phone}</Typography>
+          </Box>
         </Box>
-        <Box className="worker-details__details-item">
-          <img height="28px" src={phone} alt="Phone" />
-          <Typography variant="body1">{worker.phone}</Typography>
-        </Box>
-      </Box>
+      )}
     </Box>
   );
 };

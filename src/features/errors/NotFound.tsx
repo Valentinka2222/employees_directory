23b9a4922a524735581eab73e2c95d183d60e0cb
Valentinka2222 <xamalyan89@gmail.com>
2024-11-24 17:35:38 +0200

    added link to the erorr not found

--- src/features/errors/NotFound.tsx
+++ src/features/errors/NotFound.tsx
@@ -1,8 +1,19 @@
-import { Typography, Box } from '@mui/material';
+import { Typography, Box, Link } from '@mui/material';
+import { fetchWorkersAction } from '../../redux/reducer/employeersReducer';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { AppDispatch } from '../../redux/store/store';
 
 import './error.scss';
 
 const ErrorNotFound = () => {
+  const dispatch: AppDispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleReload = () => {
+    dispatch(fetchWorkersAction());
+    navigate('/');
+  };
   return (
     <Box className="error-box">
       <img src="/images/icon_search.png" alt="No workers found" className="error-box__image" />
@@ -22,6 +33,14 @@ const ErrorNotFound = () => {
       >
         Try to adjust your request
       </Typography>
+      <Link
+        onClick={() => handleReload()}
+        component="button"
+        underline="none"
+        className="error-box__reload-link"
+      >
+        Return to the list
+      </Link>
     </Box>
   );
 };
